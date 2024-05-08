00fb53574d442a12558d82299a39930562702001
prokawsar <prokawsar@gmail.com>
2024-05-08 16:24:06 +0600

    Fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,4 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
@@ -18,24 +15,11 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        {/* <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
-        </div> */}
-        {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <h1 className="text-2xl font-bold text-slate-600">Todo</h1>
-            <AuthButton />
-          </div>
-        </nav> */}
-      </div>
+      <div className="w-full"></div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          {/* <FetchDataSteps /> */}
-        </main>
+        <main className="flex-1 flex flex-col gap-6"></main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
