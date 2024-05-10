5f16df42a03b2ed353648eb9fa4c5a1b177886be
prokawsar <prokawsar@gmail.com>
2024-05-10 10:08:46 +0600

    Application not working

--- app/dashboard/add-category.tsx
+++ app/dashboard/add-category.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CloseButton from "@/components/CloseButton";
 import { useUserStore } from "@/store";
 import { createClient } from "@/utils/supabase/client";
 import { faMultiply } from "@fortawesome/free-solid-svg-icons";
@@ -55,17 +56,11 @@ export default function AddCategory() {
             <div className="flex flex-row items-center justify-between gap-3">
               <button
                 type="submit"
-                className="my-1 bg-purple-400 hover:bg-purple-600 text-white border border-blue-700 px-3 rounded-md"
+                className="my-1 bg-purple-400 hover:bg-purple-600 text-white border border-blue-700 px-3 rounded"
               >
                 Add
               </button>
-              <button
-                type="button"
-                className="bg-slate-200 p-1 hover:bg-slate-400 hover:text-white rounded-full h-6 w-6 flex justify-center items-center"
-                onClick={() => setShowAddCategory(false)}
-              >
-                <FontAwesomeIcon icon={faMultiply} />
-              </button>
+              <CloseButton onClick={() => setShowAddCategory(false)} />
             </div>
           </form>
         </div>
